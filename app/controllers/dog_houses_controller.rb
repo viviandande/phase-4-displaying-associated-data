f3cd3bd4afd70d4c4ba1fb5e7ec86099df244d04
+ <+>
2023-03-21 16:35:12 +0300

    Completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,9 +1,13 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  # def index
+  #   render json: DogHouse.all, except: [:created_at, :updated_at]
+  # end
+
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house
+    render json: dog_house, include: :reviews, except: [:created_at, :updated_at]
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,11 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews
+    render json: reviews, include: :dog_house
   end
 
+  # def show
+  #   review = Review.find(params[:id])
+  #   render json: review, include: :dog_house
+  # end
 end
